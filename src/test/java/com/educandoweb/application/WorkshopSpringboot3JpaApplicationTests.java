9efeae1c9e3ffa24814a478672f3950829b525cc
Lucas Marchesoti Franco <lmarchesoti@hotmail.com>
2024-11-19 16:41:13 -0300

    Update main package name

--- src/test/java/com/educandoweb/application/WorkshopSpringboot3JpaApplicationTests.java
+++ src/test/java/com/educandoweb/application/WorkshopSpringboot3JpaApplicationTests.java
@@ -1,4 +1,4 @@
-package com.educandoweb.workshop_springboot3_jpa;
+package com.educandoweb.application;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
