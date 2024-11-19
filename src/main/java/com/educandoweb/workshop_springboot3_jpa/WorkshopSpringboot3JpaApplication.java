9da2355a6df74bcdbde1153d6f35d5859e646f87
Lucas Marchesoti Franco <lmarchesoti@hotmail.com>
2024-11-19 16:37:53 -0300

    Projeto criado

---
+++ src/main/java/com/educandoweb/workshop_springboot3_jpa/WorkshopSpringboot3JpaApplication.java
@@ -0,0 +1,13 @@
+package com.educandoweb.workshop_springboot3_jpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkshopSpringboot3JpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkshopSpringboot3JpaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/educandoweb/workshop_springboot3_jpa/WorkshopSpringboot3JpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.educandoweb.workshop_springboot3_jpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopSpringboot3JpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
